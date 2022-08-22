a131237d6ebc066ed755e29170329807b2a4efc0
Christa <christanewing47@gmail.com>
2022-08-22 15:07:42 -0400

    populate generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,4 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+
+const { link } = require("fs");
+
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
@@ -13,7 +16,37 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ### ${data.description}
 
+  ## Tools:
+  *${data.languages}
+
+  ## How to Install ${data.title}:
+  ${data.installation}
+
+  ## Instructions on Installation:
+  ${data.instructions}
+
+  ## How to Use the App
+  ${data.usage}
+
+  ## Licences:
+  ###${data.license}
+
+  ## Need More Info?
+
+  ### Visit the repository with the link below
+  ### ${data.links}
+
+  ## Created By ${data.name}
+  ### About ${data.name}:
+  ${data.about}.
+
+  ## Questions?
+  You can find this project on GitHub here: https://github.com/${data.github}/ . Any further questions can be sent to the creator directly through email or phone.
+*${data.email}
+*${data.phone}
 `;
 }
 
